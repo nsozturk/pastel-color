ecf746219ace51ccb1d8baf22339a1187c455f2e
enes öztürk <e.nesozturk@icloud.com>
2023-03-28 16:27:11 +0300

    my early ios project.just added the cobebase.

---
+++ RandomColors/UIColor+Ext.swift
@@ -0,0 +1,46 @@
+//
+//  UIColor+Ext.swift
+//  RandomColors
+//
+//  Created by enes öztürk on 5.12.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static func random() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+}
+
+
+
+extension UIColor{
+    static func generateRandomPastelColor(withMixedColor mixColor: UIColor?) -> UIColor {
+        // Randomly generate number in closure
+        let randomColorGenerator = { ()-> CGFloat in
+            CGFloat(arc4random() % 256 ) / 256
+        }
+        
+        var red: CGFloat = randomColorGenerator()
+        var green: CGFloat = randomColorGenerator()
+        var blue: CGFloat = randomColorGenerator()
+        
+        // Mix the color
+        if let mixColor = mixColor {
+            var mixRed: CGFloat = 0, mixGreen: CGFloat = 0, mixBlue: CGFloat = 0;
+            mixColor.getRed(&mixRed, green: &mixGreen, blue: &mixBlue, alpha: nil)
+            
+            red = (red + mixRed) / 2;
+            green = (green + mixGreen) / 2;
+            blue = (blue + mixBlue) / 2;
+        }
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+}
+
